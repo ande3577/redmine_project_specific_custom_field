7eee18ecf63918d2250d0b73dc380c236ec66b05
ande3577 <ande3577@gmail.com>
2013-07-30 22:11:02 -0500

    Hide tab if issue tracking not enabled on project

--- lib/project_specific_field_projects_helper_patch.rb
+++ lib/project_specific_field_projects_helper_patch.rb
@@ -17,7 +17,7 @@ module ProjectSpecificFieldProjectsHelperPatch
   
   def project_settings_tabs_with_project_specific_tab
     tabs = project_settings_tabs_without_project_specific_tab
-    tabs << {:name => 'custom_fields', :action => :manage_project_activities, :partial => 'projects/settings/custom_fields', :label => :label_custom_field_plural} if User.current.allowed_to?(:manage_project_custom_fields, @project)
+    tabs << {:name => 'custom_fields', :action => :manage_project_activities, :partial => 'projects/settings/custom_fields', :label => :label_custom_field_plural} if User.current.allowed_to?(:manage_project_custom_fields, @project) and @project.module_enabled?(:issue_tracking)
     tabs
   end
   
